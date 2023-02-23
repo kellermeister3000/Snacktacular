9778d4f50844dd2143cd0f3fa75a2d0099bb65a3
Philip Keller <philipjoelkeller@gmail.com>
2023-02-23 15:36:08 -0800

    Using the Keyboard to Move between TextFields - YouTube 8.3 (Deduped SignUp, Added Login)

--- Snacktacular/LoginView.swift
+++ Snacktacular/LoginView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 import Firebase
 
 struct LoginView: View {
+    enum Field {
+        case email, password
+    }
+    
     @State private var email = ""
     @State private var password = ""
     @State private var showingAlert = false
     @State private var alertMessage = ""
+    @State private var buttonsDisabled = true
+    @FocusState private var focusField: Field?
     
     var body: some View {
         NavigationStack {
@@ -27,10 +33,24 @@ struct LoginView: View {
                     .autocorrectionDisabled()
                     .textInputAutocapitalization(.never)
                     .submitLabel(.next)
+                    .focused($focusField, equals: .email) // This Field is bound to the .email case
+                    .onSubmit {
+                        focusField = .password
+                    }
+                    .onChange(of: email) { _ in
+                        enableButtons()
+                    }
                 
                 SecureField("Password", text: $password)
                     .textInputAutocapitalization(.never)
                     .submitLabel(.done)
+                    .focused($focusField, equals: .password) // This Field is bound to the .password case
+                    .onSubmit {
+                        focusField = nil // will dismiss the keyboard
+                    }
+                    .onChange(of: password) { _ in
+                        enableButtons()
+                    }
             }
             .textFieldStyle(.roundedBorder)
             .overlay {
@@ -54,6 +74,7 @@ struct LoginView: View {
                 }
 
             }
+            .disabled(buttonsDisabled)
             .buttonStyle(.borderedProminent)
             .tint(Color("SnackColor"))
             .font(.title2)
@@ -64,6 +85,12 @@ struct LoginView: View {
         .alert(alertMessage, isPresented: $showingAlert) {
             Button("OK", role: .cancel) {}
         }
+    }
+    
+    func enableButtons() {
+        let emailIsGood = email.count >= 6 && email.contains("@")
+        let passwordIsGood = password.count >= 6
+        buttonsDisabled = !(emailIsGood && passwordIsGood)
     }
     
     func register() {
@@ -80,7 +107,7 @@ struct LoginView: View {
     }
     
     func login() {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if let error = error { // login error occured
                 print("😡LOGIN ERROR: \(error.localizedDescription)")
                 alertMessage = "LOGIN ERROR: \(error.localizedDescription)"
