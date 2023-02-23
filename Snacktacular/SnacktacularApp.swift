f44c72d8098cadff57435cba94181fd716ea89c8
Philip Keller <philipjoelkeller@gmail.com>
2023-02-23 14:12:32 -0800

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Philip Keller on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
