ada59ee51e113f1079dcdbe9efcfb81075368a52
Philip Keller <philipjoelkeller@gmail.com>
2023-02-28 11:23:04 -0800

    Adding a PhotoPicker - YouTube 8.16

--- Snacktacular/Views/SpotDetailView.swift
+++ Snacktacular/Views/SpotDetailView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import MapKit
 import FirebaseFirestoreSwift
+import PhotosUI
 
 struct SpotDetailView: View {
     struct Annotation: Identifiable {
@@ -28,6 +29,7 @@ struct SpotDetailView: View {
     @State private var showingAsSheet = false
     @State private var mapRegion = MKCoordinateRegion()
     @State private var annotations: [Annotation] = []
+    @State private var selectedPhoto: PhotosPickerItem?
     var avgRating: String {
         guard reviews.count != 0 else {
             return "-.-"
@@ -63,6 +65,62 @@ struct SpotDetailView: View {
                 annotations = [Annotation(name: spot.name, address: spot.address, coordinate: spot.coordinate)]
                 mapRegion.center = spot.coordinate
             }
+           
+            HStack {
+                Group {
+                    Text("Avg. Rating:")
+                        .font(.title2)
+                        .bold()
+                    Text(avgRating)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(Color("SnackColor"))
+                }
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+                
+                Spacer()
+                
+                Group {
+                    PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
+                        Image(systemName: "photo")
+                        Text("Photo")
+                    }
+                    .onChange(of: selectedPhoto) { newValue in
+                        Task {
+                            do {
+                                if let data = try await
+                                    newValue?.loadTransferable(type: Data.self) {
+                                    if let uiImage = UIImage(data: data) {
+                                        //TODO: This is where you'd set your Image = Image(uiImage: uiImage) or call your function to save the image
+                                        print("📸 Successfully selected image!")
+                                    }
+                                }
+                            } catch {
+                                print("😡 ERROR: selecting image failed \(error.localizedDescription)")
+                            }
+                        }
+                    }
+                    
+                    Button(action: {
+                        if spot.id == nil {
+                            showSaveAlert.toggle()
+                        } else {
+                            showReviewViewSheet.toggle()
+                        }
+                    }, label: {
+                        Image(systemName: "star.fill")
+                        Text("Rate")
+                    })
+                }
+                .font(Font.caption)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+                .buttonStyle(.borderedProminent)
+                .bold()
+                .tint(Color("SnackColor"))
+            }
+            .padding(.horizontal)
             
             List {
                 Section {
@@ -74,29 +132,7 @@ struct SpotDetailView: View {
                         }
                         
                     }
-                } header: {
-                    HStack {
-                        Text("Avg. Rating:")
-                            .font(.title2)
-                            .bold()
-                        Text(avgRating)
-                            .font(.title)
-                            .fontWeight(.black)
-                            .foregroundColor(Color("SnackColor"))
-                        Spacer()
-                        Button("Rate It") {
-                            if spot.id == nil {
-                                showSaveAlert.toggle()
-                            } else {
-                                showReviewViewSheet.toggle()
-                            }
-                        }
-                        .buttonStyle(.borderedProminent)
-                        .bold()
-                        .tint(Color("SnackColor"))
-                    }
                 }
-                .headerProminence(.increased)
             }
             .listStyle(.plain)
             
